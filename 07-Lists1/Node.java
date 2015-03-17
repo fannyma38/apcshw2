b9ef6203165490b8034bb046bda3ec9ade334dd6
fannyma38 <fannyma38@gmail.com>
2015-03-17 06:03:46 -0400

    Update Node.java

--- 07-Lists1/Node.java
+++ 07-Lists1/Node.java
@@ -1,59 +1,22 @@
-public class LList {
-    private Node l=null;
-
-    public void add(String s){
-	Node tmp = new Node(s);
-	tmp.setNext(l);
-	l = tmp;
-    }
-
-    public String toString(){
-	String s = "";
-	Node tmp;
-	for (tmp = l; tmp != null; tmp=tmp.getNext()){
-	    s = s + tmp + " --> ";
-	}
-	s = s + "null";
-	return s;
-    }
-
-        public int find(String s){
-	Node temp = l;
-	int i = 0;
-	while (temp != null){
-	    if (temp.getData().equals(s)){
-		return i;
-	    }
-	    temp = temp.getNext();
-	    i++;
-	}
-	return -1;
-    }
-
-
-    public void add(int i, String s){
-	if (i==0){
-	    add(s);
-	}
-	else {
-	    Node temp = l;
-	    for (int x = 0; x < i - 1; x++){
-		temp = temp.getNext();
-	    }
-	    Node n = new Node(s);
-	    n.setNext(temp.getNext());
-	    temp.setNext(n);
-	}
-    }
-
-
-    public static void main(String[] args) {
-	LList ret = new LList();
-	ll.add("Fri");
-	ll.add("Wed");
-	System.out.println(ret.find(0));
-    }
-
-    
-		
+public class Node {
+		private String data;
+		private Node next;
+		public Node(String s){
+				data = s;
+				next = null;
+		}
+		public void setData(String s){
+				data = s;
+		}
+		public String getData() {
+				return data;
+		}
+		public void setNext(Node n){
+				next = n;
+		}
+		public Node getNext(){
+				return next;
+		}
+		public String toString() {
+				return data;}
 }
