108e3512906706929ff8ce0d5a2d603f37aa1acf
fannyma38 <fannyma38@gmail.com>
2015-03-26 08:37:11 -0400

    .

--- 11-Queues/Node.java
+++ 11-Queues/Node.java
@@ -25,3 +25,5 @@ public class Node<E>{
 	return ""+data;
     }
 }
+
+ 
---
+++ 11-Queues/queuearray.java
@@ -0,0 +1,30 @@
+public class queuearray<E> {
+
+    private ArrayList<E> n;
+    private int backindex;
+    private int frontindex;
+    private int len;
+    
+    public queuearray() {
+	n = new ArrayList<E>();
+	frontindex = 0;
+	backindex = frontindex;
+	len = 0;
+    }
+
+    public void enqueue(E data) {
+	
+    }
+
+    public E dequeue() {
+
+    }
+
+    public boolean empty() {
+
+    }
+
+    public E front() {
+
+    }
+}
