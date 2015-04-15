fcad103d061fab322d0883919766ec5edefbf94d
fannyma38 <fannyma38@gmail.com>
2015-04-15 08:16:01 -0400

    Update Node.java

--- 12-Maze2/Node.java
+++ 12-Maze2/Node.java
@@ -1,27 +1,21 @@
-public class Node<E>{
-    private E data;
-    private Node<E> next;
-    public Node(){
-	data = null;
-	next = null;
-    }
-    public Node(E s){
-	data = s;
-	next = null;
-    }
-    public void setData(E s){
-	data = s;
-    }
-    public E getData(){
-	return data;
-    }
-    public void setNext(Node<E> n){
-	next = n;
-    }
-    public Node<E> getNext(){
-	return next;
-    }
-    public String toString(){
-	return ""+data;
-    }
-}
+	public class Node{
+	//not the same as linkedlist node
+	private int x,y;
+	private Node prev;
+	public Node(int x, int y){
+	this.x = x;
+	this.y = y;
+	}
+	public int getx(){
+	return x;
+	}
+	public int gety(){
+	return y;
+	}
+	public Node getPrev(){
+	return prev;
+	}
+	public void setPrev(Node n){
+	prev = n;
+	}
+	}
